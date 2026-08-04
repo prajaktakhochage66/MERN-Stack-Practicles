f5af4ec7c3804cfc5bc4b4a00e3d8326f7e38bcc
prajaktakhochage66 <138974607+prajaktakhochage66@users.noreply.github.com>
2026-08-04 12:36:11 +0530

    Created Login Page UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleLogin(e) {
+    e.preventDefault();
+
+    if (email === "" || password === "") {
+      alert("Please fill all fields");
+    } else {
+      alert("Login Successful");
+      console.log("Email:", email);
+      console.log("Password:", password);
+
+      setEmail("");
+      setPassword("");
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="login-box">
+
+        <h1>Login Page</h1>
+
+        <form onSubmit={handleLogin}>
+
+          <label>Email</label>
+
+          <input
+            type="email"
+            placeholder="Enter Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
+          <label>Password</label>
+
+          <input
+            type="password"
+            placeholder="Enter Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <button type="submit">
+            Login
+          </button>
+
+        </form>
+
+      </div>
     </div>
   );
 }
